07c9d09bc18c8b052fbd4e8eaca93b3966e57f82
ShlokMarne <95908743+ShlokMarne@users.noreply.github.com>
2025-02-17 21:15:12 +0530

    List indexing

---
+++ listprog2.py
@@ -0,0 +1,6 @@
+list1 = ["New Delhi","Mumbai","Banglore","Pune"]
+print("List elements are:",list1)
+print("Elements @ 0 index:",list1[0])
+print("Elements @ 1 index:",list1[1])
+print("Elements @ 2 index:",list1[2])
+print("Elements @ 3 index:",list1[3])
