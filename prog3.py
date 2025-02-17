ab84185dab26dbe967c7511f373fa0514ad7f9ba
ShlokMarne <95908743+ShlokMarne@users.noreply.github.com>
2025-02-17 20:56:12 +0530

    Lenght

---
+++ prog3.py
@@ -0,0 +1,6 @@
+list = ["abnbhbbc","fgte","qwrty","mkloij","qwertyu","zx","z","qwertyuiooplk","qazwsxedcrfvtgbyhnujmj"]
+output_list = []
+for x in list:
+    output_list.append(len(x))
+print(list)
+print(output_list)
